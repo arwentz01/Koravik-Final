3368c8f5ed7057fb2adbdd31733c0c400cc89c07
arwentz01 <arwentz01@gmail.com>
2026-07-28 14:00:18 -0400

    Build 009: register notification event consumer

--- tools/worker.php
+++ tools/worker.php
@@ -7,12 +7,14 @@ require dirname(__DIR__) . '/bootstrap.php';
 use Koravik\Platform\Events\CompositeConsumer;
 use Koravik\Platform\Events\OutboxWorker;
 use Koravik\Platform\Experience\ExperienceConsumer;
+use Koravik\Platform\Notifications\NotificationConsumer;
 use Koravik\Worlds\EpicOrdinary\EpicOrdinaryConsumer;
 
 $limit = isset($argv[1]) ? (int) $argv[1] : 10;
 $consumer = new CompositeConsumer([
     new ExperienceConsumer(database()),
     new EpicOrdinaryConsumer(database()),
+    new NotificationConsumer(database()),
 ]);
 $result = (new OutboxWorker(database(), $consumer))->run($limit);
 
