61f762967f20f27d028222b9dad2d95971898973
arwentz01 <arwentz01@gmail.com>
2026-07-28 11:58:19 -0400

    Build 004: run all platform event consumers

--- tools/worker.php
+++ tools/worker.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 require dirname(__DIR__) . '/bootstrap.php';
 
+use Koravik\Platform\Events\CompositeConsumer;
 use Koravik\Platform\Events\OutboxWorker;
+use Koravik\Platform\Experience\ExperienceConsumer;
 use Koravik\Worlds\EpicOrdinary\EpicOrdinaryConsumer;
 
 $limit = isset($argv[1]) ? (int) $argv[1] : 10;
-$result = (new OutboxWorker(database(), new EpicOrdinaryConsumer(database())))->run($limit);
+$consumer = new CompositeConsumer([
+    new ExperienceConsumer(database()),
+    new EpicOrdinaryConsumer(database()),
+]);
+$result = (new OutboxWorker(database(), $consumer))->run($limit);
 
 echo json_encode($result, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR) . PHP_EOL;
