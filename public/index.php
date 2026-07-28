35b083e671d838908f4b608966fa2f9a39f1df96
arwentz01 <arwentz01@gmail.com>
2026-07-28 13:42:07 -0400

    Build 008: route return experience before application shell

--- public/index.php
+++ public/index.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 require dirname(__DIR__) . '/bootstrap.php';
 
+Koravik\Platform\Security\Security::startSession();
+
+$returnController = new Koravik\Platform\ReturnExperience\ReturnController(database());
+if ($returnController->handle()) {
+    return;
+}
+
 $method = strtoupper($_SERVER['REQUEST_METHOD'] ?? 'GET');
 $path = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?: '/';
 
